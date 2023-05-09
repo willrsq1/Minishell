bf27ca6311d9b79356d5f35b3051f76ea6062218
william <william.ruetsuquet@gmail.com>
2023-05-09 04:09:51 +0200

    crash_test update

--- crash_test.sh
+++ crash_test.sh
@@ -13,8 +13,13 @@ sleep 1
 cat /dev/urandom | valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ./minishell 2>>err.txt 1>>err.txt &
 
 # Step 3
+
+echo "Please wait 20 seconds."
 sleep 10
-echo "exit"
+echo "Please wait another 10 secs..."
+sleep 10
+echo "exit" >/dev/null 2>&1
+echo "Done !"
 
 # Step 4
 grep -an "signal" err.txt >> results.txt || echo "No Segfault !" >> results.txt
@@ -22,7 +27,8 @@ grep -an "0x1" err.txt | grep -av "main" >> results.txt || echo "No leaks !" >> 
 grep -an "open" err.txt | grep -av "2 open" | grep -av "3 open" >> results.txt || echo "No unclosed files !" >> results.txt
 grep -an "Invalid" err.txt >>results.txt || echo "No Invalid read/write !" >> results.txt
 
-echo "input 'cat results.txt' to see your results" 1>&1
+echo "input 'cat results.txt' to see your results."
+echo "The line of an error is at the beginning of a line."
+echo "Use 'tail -n +LIGN_NB err.txt | head -n 20' to see the full error."
 
-# Clean up
 
